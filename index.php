883e65c3943b2d8ac21fe458a54af945718be982
Cameron Epstein <cameron.a.epstein@gmail.com>
2018-05-30 08:51:09 +0100

    create equity_securities object

--- index.php
+++ index.php
@@ -2,13 +2,16 @@
 include(dirname(__FILE__) . "/alphaVantage/index.php");
 // ADD desired securities and Alpha Vantage API functions here:
 $equity_securities = [
-  "MSFT" => "TIME_SERIES_INTRADAY"
+  "MSFT" => [
+    "function" => "TIME_SERIES_INTRADAY",
+    "outputsize" => "compact",
+  ],
 ];
 // Run api requests for each security
-foreach($equity_securities as $security => $api_function) {
+foreach($equity_securities as $security => $api_params) {
   $res["security"] = $security;
-  $res["data_type"] = $api_function;
-  $res["data"] = requestAVAPI($security, $api_function);
+  $res["data_type"] = $api_params["function"];
+  $res["data"] = requestAVAPI($security, $api_params);
   print_r($res["data"]);
 }
 ?>
