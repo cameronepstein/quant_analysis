f87b1cf9ed8c83fd383de802685e5f9f01784e13
Cameron Epstein <cameron.a.epstein@gmail.com>
2018-06-06 09:12:07 +0100

    insert moving average data to new table

--- db/queryGenerator.php
+++ db/queryGenerator.php
@@ -3,7 +3,7 @@ function generateQuery($data) {
   if ($data['data_type'] === 'TIME_SERIES_INTRADAY') {
     $query = 'INSERT INTO c_test.time_series_intraday (security, last_refreshed, time_zone, timestamp, open, high, low, close, volume) VALUES '.$data['csv_formatted_data'].'ON DUPLICATE KEY UPDATE open = open, high=high, low=low, close=close, volume=volume';
   } else if ($data['data_type'] === 'SMA' || $data['data_type'] === 'EMA' || $data['data_type'] === 'WMA') {
-    $query = 'INSERT INTO c_test.'.$data['data_type'].' (security, technical_indicator, time_interval, time_period, series_type, time_zone, last_refreshed, recorded_time, value) VALUES'.$data['csv_formatted_data'].'ON DUPLICATE KEY UPDATE value=value';
+    $query = 'INSERT INTO c_test.moving_averages (security, technical_indicator, time_interval, time_period, series_type, time_zone, last_refreshed, recorded_time, value) VALUES'.$data['csv_formatted_data'].'ON DUPLICATE KEY UPDATE value=value';
   }
   return $query;
 }
