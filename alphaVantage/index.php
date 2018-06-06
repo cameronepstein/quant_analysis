7d56a1260b8a00c2fed6e10d75603562b2b1a3db
Cameron Epstein <cameron.a.epstein@gmail.com>
2018-06-06 09:03:33 +0100

    pull WMA data

--- alphaVantage/index.php
+++ alphaVantage/index.php
@@ -2,7 +2,7 @@
 function requestAVApi($security, $api_params) {
   include(dirname(__FILE__) . '/../config.php');
   $url = 'https://www.alphavantage.co/query?function='.$api_params['function'].'&symbol='.$security.'&interval=1min&outputsize='.$api_params['outputsize'];
-  if ($api_params['function'] === 'SMA' || $api_params['function'] === 'EMA') {
+  if ($api_params['function'] === 'SMA' || $api_params['function'] === 'EMA' || $api_params['function'] === 'WMA') {
     $url .= '&time_period='.$api_params['time_period'].'&series_type='.$api_params['series_type'];
   }
   $url .= '&apikey='.$alpha_vantage_key;
