6caf8e11221f5d32c7e4d2815267813c7f199372
Cameron Epstein <cameron.a.epstein@gmail.com>
2018-06-27 08:34:56 +0100

    pull tema data and update avapi function

--- alphaVantage/index.php
+++ alphaVantage/index.php
@@ -2,8 +2,11 @@
 function requestAVApi($security, $api_params) {
   include(dirname(__FILE__) . '/../config.php');
   $url = 'https://www.alphavantage.co/query?function='.$api_params['function'].'&symbol='.$security.'&interval=1min&outputsize='.$api_params['outputsize'];
-  if ($api_params['function'] === 'SMA' || $api_params['function'] === 'EMA' || $api_params['function'] === 'WMA' || $api_params['function'] === 'DEMA') {
-    $url .= '&time_period='.$api_params['time_period'].'&series_type='.$api_params['series_type'];
+  if (isset($api_params['time_period'])) {
+    $url .= '&time_period='.$api_params['time_period'];
+  }
+  if (isset($api_params['series_type'])) {
+    $url .= '&series_type='.$api_params['series_type'];
   }
   $url .= '&apikey='.$alpha_vantage_key;
   $data = file_get_contents($url);
