e2acb4f17b13c721306f59c66a28a242c4a30a2a
Cameron Epstein <cameron.a.epstein@gmail.com>
2018-06-28 08:40:46 +0100

    update requestAvApi function to create url from api_params

--- alphaVantage/index.php
+++ alphaVantage/index.php
@@ -1,12 +1,9 @@
 <?php
-function requestAVApi($security, $api_params) {
+function requestAVApi($api_params) {
   include(dirname(__FILE__) . '/../config.php');
-  $url = 'https://www.alphavantage.co/query?function='.$api_params['function'].'&symbol='.$security.'&interval=1min&outputsize='.$api_params['outputsize'];
-  if (isset($api_params['time_period'])) {
-    $url .= '&time_period='.$api_params['time_period'];
-  }
-  if (isset($api_params['series_type'])) {
-    $url .= '&series_type='.$api_params['series_type'];
+  $url = 'https://www.alphavantage.co/query?';
+  foreach($api_params as $key => $val) {
+    $url .= '&'.$key.'='.$val;
   }
   $url .= '&apikey='.$alpha_vantage_key;
   $data = file_get_contents($url);
--- index.php
+++ index.php
@@ -10,75 +10,83 @@ include(dirname(__FILE__) . '/dataFormatter/dataFormatter.php');
 // ADD desired securities and Alpha Vantage API functions here:
 $equity_securities = [
   [
-    'security' => 'MSFT',
     'function' => 'TIME_SERIES_INTRADAY',
+    'symbol' => 'MSFT',
+    'interval' => '1min',
     'outputsize' => 'full',
     'db_table' => 'time_series_intraday' // NOT PART OF THE AV API. For project insert queries
   ],
   [
-    'security' => 'MSFT',
     'function' => 'SMA',
+    'symbol' => 'MSFT',
+    'interval' => '1min',
     'outputsize' => 'full',
     'time_period' => '10', // Number of data points used to calculate each moving average value.
     'series_type' => 'close', // The desired price type in the time series. Four types are supported: can be open, close, high or low
     'db_table' => 'moving_averages' // NOT PART OF THE AV API. For project insert queries
   ],
   [
-    'security' => 'MSFT',
     'function' => 'EMA',
+    'symbol' => 'MSFT',
+    'interval' => '1min',
     'outputsize' => 'full',
     'time_period' => '10', // Number of data points used to calculate each moving average value.
     'series_type' => 'close', // The desired price type in the time series. Four types are supported: can be open, close, high or low
     'db_table' => 'moving_averages' // NOT PART OF THE AV API. For project insert queries
   ],
   [
-    'security' => 'MSFT',
     'function' => 'WMA',
+    'symbol' => 'MSFT',
+    'interval' => '1min',
     'outputsize' => 'full',
     'time_period' => '10', // Number of data points used to calculate each moving average value.
     'series_type' => 'close', // The desired price type in the time series. Four types are supported: can be open, close, high or low
     'db_table' => 'moving_averages' // NOT PART OF THE AV API. For project insert queries
   ],
   [
-    'security' => 'MSFT',
     'function' => 'DEMA',
+    'symbol' => 'MSFT',
+    'interval' => '1min',
     'outputsize' => 'full',
     'time_period' => '10', // Number of data points used to calculate each moving average value.
     'series_type' => 'close', // The desired price type in the time series. Four types are supported: can be open, close, high or low
     'db_table' => 'moving_averages' // NOT PART OF THE AV API. For project insert queries
   ],
   [
-    'security' => 'MSFT',
     'function' => 'TEMA',
+    'symbol' => 'MSFT',
+    'interval' => '1min',
     'outputsize' => 'full',
     'time_period' => '10', // Number of data points used to calculate each moving average value.
     'series_type' => 'close', // The desired price type in the time series. Four types are supported: can be open, close, high or low
     'db_table' => 'moving_averages' // NOT PART OF THE AV API. For project insert queries
   ],
   [
-    'security' => 'MSFT',
     'function' => 'TRIMA',
+    'symbol' => 'MSFT',
+    'interval' => '1min',
     'outputsize' => 'full',
     'time_period' => '10', // Number of data points used to calculate each moving average value.
     'series_type' => 'close', // The desired price type in the time series. Four types are supported: can be open, close, high or low
     'db_table' => 'moving_averages' // NOT PART OF THE AV API. For project insert queries
   ],
   [
-    'security' => 'MSFT',
     'function' => 'KAMA',
+    'symbol' => 'MSFT',
+    'interval' => '1min',
     'outputsize' => 'full',
     'time_period' => '10', // Number of data points used to calculate each moving average value.
     'series_type' => 'close', // The desired price type in the time series. Four types are supported: can be open, close, high or low
     'db_table' => 'moving_averages' // NOT PART OF THE AV API. For project insert queries
-  ]
+  ],
 ];
 // Run api requests for each security
 foreach($equity_securities as $api_params) {
-  $res['security'] = $api_params['security'];
+  $res['security'] = $api_params['symbol'];
   $res['data_type'] = $api_params['function'];
   $res['output_size'] = $api_params['outputsize'];
   $res['db_table'] = $api_params['db_table'];
-  $res['data'] = requestAVAPI($res['security'], $api_params);
+  $res['data'] = requestAVAPI($api_params);
   // print_r($res['data']);
   $res['csv_formatted_data'] = formatData($res);
   insertToDb($res);
